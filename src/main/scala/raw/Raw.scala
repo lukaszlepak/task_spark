ed2b637d50184508aef73eef1fa6c23c52a3dd51
lukasz <lukasz.lepak@protonmail.com>
2025-06-16 22:14:24 +0200

    add schema validation

--- src/main/scala/raw/Raw.scala
+++ src/main/scala/raw/Raw.scala
@@ -1,11 +1,49 @@
 package raw
 
+import org.apache.spark.sql.types.{ArrayType, DateType, DecimalType, StringType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Raw {
-  def loadTransactions(path: String)(implicit spark: SparkSession): DataFrame = spark.read.parquet(path)
+  def loadTransactions(path: String)(implicit spark: SparkSession): DataFrame = {
+    val transactionSchema = StructType(Seq(
+      StructField("TRANSACTION_ID", StringType),
+      StructField("GUARANTORS", ArrayType(
+        StructType(Seq(
+          StructField("NAME", StringType),
+          StructField("PERCENTAGE", DecimalType.SYSTEM_DEFAULT),
+        )))),
+      StructField("SENDER_NAME", StringType),
+      StructField("RECEIVER_NAME", StringType),
+      StructField("TYPE", StringType),
+      StructField("COUNTRY", StringType),
+      StructField("CURRENCY", StringType),
+      StructField("AMOUNT", DecimalType.SYSTEM_DEFAULT),
+      StructField("STATUS", StringType),
+      StructField("DATE", DateType),
+      StructField("PARTITION_DATE", StringType)
+    ))
 
-  def loadCurrenciesRegistry(path: String)(implicit spark: SparkSession): DataFrame = spark.read.parquet(path)
+    spark.read.schema(transactionSchema).parquet(path)
+  }
 
-  def loadReceiversRegistry(path: String)(implicit spark: SparkSession): DataFrame = spark.read.parquet(path)
+  def loadCurrenciesRegistry(path: String)(implicit spark: SparkSession): DataFrame = {
+    val currencySchema = StructType(Seq(
+      StructField("FROM_CURRENCY", StringType),
+      StructField("TO_CURRENCY", StringType),
+      StructField("RATE", DecimalType.SYSTEM_DEFAULT),
+      StructField("PARTITION_DATE", StringType)
+    ))
+
+    spark.read.schema(currencySchema).parquet(path)
+  }
+
+  def loadReceiversRegistry(path: String)(implicit spark: SparkSession): DataFrame = {
+    val registrySchema = StructType(Seq(
+      StructField("RECEIVER_NAME", StringType),
+      StructField("COUNTRY", StringType),
+      StructField("PARTITION_DATE", StringType)
+    ))
+
+    spark.read.schema(registrySchema).parquet(path)
+  }
 }
